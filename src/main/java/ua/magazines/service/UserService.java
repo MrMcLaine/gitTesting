221074fa5ba887b6d39bdee370a77ed05aa4f716
MrMcLaine <Lypovskyi@gmail.com>
2023-03-11 01:31:12 +0200

    Add entity.User & service.UserService

---
+++ src/main/java/ua/magazines/service/UserService.java
@@ -0,0 +1,33 @@
+package ua.magazines.service;
+
+import ua.magazines.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    List<User> listOfUsers = new ArrayList<>();
+    private static UserService userService;
+
+    private UserService() {
+    }
+
+    public static UserService getUserService() {
+        if (userService == null) {
+            userService = new UserService();
+        }
+        return userService;
+    }
+
+    public List<User> getListOfUsers() {
+        return listOfUsers;
+    }
+
+    public void saveUser(User user) {
+        listOfUsers.add(user);
+    }
+
+    public User getUser(String email) {
+        return listOfUsers.stream().filter(u -> u.getEmail().equalsIgnoreCase(email)).findFirst().get();
+    }
+}
